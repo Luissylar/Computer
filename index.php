ae3688ee0b00c91246a7025204d21a377e54bc0f
Luis <luisfsaldivarleon@gmail.com>
2023-10-25 06:35:32 -0500

    se hizo el cambio del titulo de la pagina principal index.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/Tienda/CSS/principal.css">
-    <title>Tienda de Equipos de Cómputo</title>
+    <title>Computer Hub</title>
 
 </head>
 <body>
